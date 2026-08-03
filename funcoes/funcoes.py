823c52fd21a034e26266e389b69c44267ab72e01
Icrxmee <josepedroicaro@gmail.com>
2026-08-03 16:25:06 -0300

    08. Abstração

--- funcoes/funcoes.py
+++ funcoes/funcoes.py
@@ -1,4 +1,5 @@
 from categorias import pastas
+import time
 
 def identificar_categoria(extensao):
   return pastas.get(extensao, 'Outros')
@@ -18,3 +19,19 @@ def encontrar_nome_novo(caminho_final):
       contador += 1
     else:
        return caminho_novo
+
+def organiza_arquivo(item, downloads):
+  categoria = identificar_categoria(item.suffix)
+  caminho_categoria = downloads / categoria
+   
+  print(f"✔ {item.name} → {categoria}")
+  time.sleep(0.3)
+   
+  criar_pastas(caminho_categoria)
+   
+  caminho_final = caminho_categoria / item.name
+   
+  if caminho_final.exists():
+    caminho_final = encontrar_nome_novo(caminho_final)
+
+  item.rename(caminho_final)
--- main.py
+++ main.py
@@ -1,29 +1,15 @@
+import time
 from pathlib import Path
 from funcoes.funcoes import (
-  identificar_categoria,
-  criar_pastas,
-  encontrar_nome_novo
+  organiza_arquivo
 )
-import time
-
-downloads = Path.home() / "Downloads"
 
 print("Organizando Arquivos...")
 time.sleep(1)
 
+downloads = Path.home() / "Downloads"
+
 for item in downloads.iterdir(): 
 
   if item.is_file():
-    categoria = identificar_categoria(item.suffix)
-    caminho_categoria = downloads / categoria
-
-    print(f"✔ {item.name} → {categoria}")
-    time.sleep(0.3)
-
-    criar_pastas(caminho_categoria)
-
-    caminho_final = caminho_categoria / item.name
-
-    if caminho_final.exists():
-       caminho_final = encontrar_nome_novo(caminho_final)
-    item.rename(caminho_final)
+    organiza_arquivo(item, downloads)
